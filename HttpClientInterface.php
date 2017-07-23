7facea7dab2b89e3e564fccc10f551df76b95983
Jim Mullen <jim@idimensionz.com>
2017-07-23 19:42:39 -0400

    Add client interface, response object and meta files.

---
+++ HttpClientInterface.php
@@ -0,0 +1,111 @@
+<?php
+/*
+ * iDimensionz/{http-client}
+ * HttpClientInterface.php
+ *  
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 iDimensionz
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+namespace iDimensionz\HttpClient;
+
+interface HttpClientInterface
+{
+    /**
+     * Create a GET request for the client
+     *
+     * @param string    $uri     Resource URI
+     * @param array     $headers HTTP headers
+     * @param array     $options Options to apply to the request.
+     * @return HttpResponse
+     */
+    public function get($uri = null, $headers = null, $options = array());
+
+    /**
+     * Create a HEAD request for the client
+     *
+     * @param string    $uri     Resource URI
+     * @param array     $headers HTTP headers
+     * @param array     $options Options to apply to the request
+     *
+     * @return HttpResponse
+     */
+    public function head($uri = null, $headers = null, array $options = array());
+
+    /**
+     * Create a DELETE request for the client
+     *
+     * @param string    $uri     Resource URI
+     * @param array     $headers HTTP headers
+     * @param string    $body    Body to send in the request
+     * @param array     $options Options to apply to the request
+     *
+     * @return HttpResponse
+     */
+    public function delete($uri = null, $headers = null, $body = null, array $options = array());
+
+    /**
+     * Create a PUT request for the client
+     *
+     * @param string    $uri     Resource URI
+     * @param array     $headers HTTP headers
+     * @param string    $body    Body to send in the request
+     * @param array     $options Options to apply to the request
+     *
+     * @return HttpResponse
+     */
+    public function put($uri = null, $headers = null, $body = null, array $options = array());
+
+    /**
+     * Create a PATCH request for the client
+     *
+     * @param string    $uri     Resource URI
+     * @param array     $headers HTTP headers
+     * @param string    $body    Body to send in the request
+     * @param array     $options Options to apply to the request
+     *
+     * @return HttpResponse
+     */
+    public function patch($uri = null, $headers = null, $body = null, array $options = array());
+
+    /**
+     * Create a POST request for the client
+     *
+     * @param string    $uri     Resource URI
+     * @param array     $headers HTTP headers
+     * @param string    $body    Body to send in the request
+     * @param array     $options Options to apply to the request
+     *
+     * @return HttpResponse
+     */
+    public function post($uri = null, $headers = null, $body = null, array $options = array());
+
+    /**
+     * Create an OPTIONS request for the client
+     *
+     * @param string    $uri     Resource URI
+     * @param array     $options Options to apply to the request
+     *
+     * @return \HttpResponse
+     */
+    public function options($uri = null, array $options = array());
+}
---
+++ HttpResponse.php
@@ -0,0 +1,166 @@
+<?php
+/*
+ * iDimensionz/{http-client}
+ * HttpResponse.php
+ *  
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 iDimensionz
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+namespace iDimensionz\HttpClient;
+
+class HttpResponse
+{
+    /**
+     * @var array Array of reason phrases and their corresponding status codes
+     */
+    private static $statusTexts = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        426 => 'Upgrade required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates (Experimental)',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+    );
+
+    /** @var string The response body */
+    protected $body;
+
+    /** @var int The HTTP 1.1 status code of the response */
+    protected $statusCode;
+
+    public function __construct($statusCode = 200, $body = '')
+    {
+        $this->setStatusCode($statusCode);
+        $this->setBody($body);
+    }
+
+    /**
+     * @param int|null $statusCode
+     * @return string The description of the HTTP 1.1 status code of the response.
+     */
+    public function getReasonPhrase($statusCode = null)
+    {
+        $reasonPhrase = '';
+        $statusCode = is_null($statusCode) ? $this->getStatusCode() : (int) $statusCode;
+        if ($this->isValidStatusCode($statusCode)) {
+            $reasonPhrase = self::$statusTexts[$this->getStatusCode()];
+        }
+
+        return $reasonPhrase;
+    }
+
+    /**
+     * @param int $statusCode
+     * @return bool
+     */
+    public function isValidStatusCode($statusCode)
+    {
+        return isset(self::$statusTexts[(int)$statusCode]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     */
+    public function setBody($body)
+    {
+        $this->body = (string) $body;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param int $statusCode
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = (int) $statusCode;
+    }
+}
